3ac86590d25212bff3af2671e00b308974c259d3
Stephan Cilliers <stephanus.cilliers@gmail.com>
2017-12-25 14:52:54 +0200

    Implement buying, selling and order management

--- bittrex_utils.py
+++ bittrex_utils.py
@@ -33,25 +33,63 @@ class BittrexUtils:
 	def __init__(self):
 		self.my_bittrex = Bittrex(BITTREX_KEY, BITTREX_SECRET)
 	
-	def get_available_balance(self):
-		return self.my_bittrex.get_balance("BTC")["result"]["Balance"]
+	def get_available_balance(self, symbol):
+		return self.my_bittrex.get_balance(symbol)["result"]["Available"]
 
 	def get_ask(self, symbol):
+		"""Return current ask price for symbol"""
 		pair = f'BTC-{symbol}'
 		return self.my_bittrex.get_marketsummary(pair)["result"][0]["Ask"]
 
 	def get_bid(self, symbol):
+		"""Return current bid price for symbol"""
 		pair = f'BTC-{symbol}'
 		return self.my_bittrex.get_marketsummary(pair)["result"][0]["Bid"]
 
-	def buy_in_btc(self, symbol, amount):
+	def get_last(self, symbol):
+		"""Return current last price for symbol"""
 		pair = f'BTC-{symbol}'
-		ask = self.get_ask(symbol)
-		quantity = amount/ask
-		return pair, ask, round(quantity, 8)
-		# return self.my_bittrex.buy_limit(pair, quantity, ask)
+		return self.my_bittrex.get_marketsummary(pair)["result"][0]["Last"]
 
+	def prepare_btc_buy(self, symbol, amount):
+		"""Prepare get pair, quantity and price for create_buy_order"""
+		pair = f'BTC-{symbol}'
+		price = self.get_ask(symbol) * 1.02 # Buy 2% higher
+		quantity = round(amount/price, 8)
+		return pair, quantity, price
+
+	def create_buy_order(self, pair, quantity, price):
+		"""Create buy order on Bittrex, return order uuid"""
+		response = self.my_bittrex.buy_limit(pair, quantity, price)
+		if response["success"]:
+			return response["result"]["uuid"]
+		else:
+			raise Exception(response["message"])
+
+	def create_sell_order(self, pair, quantity, price):
+		"""Create sell order on Bittrex, return order uuid"""
+		response = self.my_bittrex.sell_limit(pair, quantity, price)
+		if response["success"]:
+			return response["result"]["uuid"]
+		else:
+			raise Exception(response["message"])
+	
+	def get_open_orders(self):
+		orders = self.my_bittrex.get_open_orders()["result"]
+		result = []
+		for order in orders:
+			message = f'Order {order["OrderUuid"]}\n\n{order["Exchange"]}\nType: {order["OrderType"]}\nQuantity: {order["Quantity"]}\nPrice: {order["Limit"]}\nBTC total: {order["Limit"]*order["Quantity"]}\n\nOpen: {order["Closed"] == None}'
+			result.append(message)
+		return result
+
+	def cancel_order(self, uuid):
+		return self.my_bittrex.cancel(uuid)["success"]
+	
+	def get_order_status(self, uuid):
+		"""Returns string of order status"""
+		order = self.my_bittrex.get_order(uuid)["result"]
+		return f'Order {order["OrderUuid"]}\n\n{order["Exchange"]}\nType: {order["Type"]}\nQuantity: {order["Quantity"]}\nPrice: {order["Limit"]}\nBTC total: {order["Reserved"]}\n\nOpen: {order["IsOpen"]}'
 
 if __name__ == "__main__":
-	utils = BittrexUtils()
-	# print(utils.buy_in_btc("doge", 0.01))
+	trex = BittrexUtils()
+	print(trex.get_order_status("e0ad333f-dc9a-4d8c-ae7e-e65f5ac223cc"))
--- telegram.py
+++ telegram.py
@@ -15,8 +15,12 @@ class TelegramBot:
 		self.order_callback = order_callback
 		self.bot = telepot.Bot(BOT_TOKEN)
 		self.bittrex_utils = BittrexUtils()
-		self.buying = None
 		self.chat_id = None
+
+		# Orders
+		self.buying = None
+		self.selling = None
+
 		MessageLoop(self.bot, {
 			"chat" : self.chat_handler,
 			"callback_query": self.callback_query_handler
@@ -41,23 +45,98 @@ class TelegramBot:
 		replies = []
 		if msg["text"] == "/start":
 			replies.append(("I will notify you when crypto people tweet and give you buying options.", None))
+		
+		# /orderstatus
+		elif msg["text"][:len("/orderstatus")] == "/orderstatus":
+			try:
+				uuid = msg["text"].split("/orderstatus ")[1].split()[0]
+				status = self.bittrex_utils.get_order_status(uuid)
+				replies.append((status, None))
+			except IndexError:
+				replies.append((f'The syntax for this command is "/orderstatus ORDER_UUID"', None))
+			except Exception as e:
+				raise e
+				replies.append((f'An error occurred: {e}', None))
+		
+		# /cancelorder
+		elif msg["text"][:len("/cancelorder")] == "/cancelorder":
+			try:
+				uuid = msg["text"].split("/cancelorder ")[1].split()[0]
+				success = self.bittrex_utils.cancel_order(uuid)
+				if not success:
+					raise Exception(f'Response returned false')
+				replies.append((f'Canceled order {uuid}.', None))
+			except IndexError:
+				replies.append((f'The syntax for this command is "/orderstatus ORDER_UUID"', None))
+			except Exception as e:
+				replies.append((f'Could not cancel order {uuid}. (Exception: {e})', None))
+		
+		# /getopenorders
+		elif msg["text"][:len("/getopenorders")] == "/getopenorders":
+			try:
+				orders = self.bittrex_utils.get_open_orders()
+				[replies.append((x, None)) for x in orders]
+				if len(orders) == 0:
+					replies.append(("No open orders.", None))
+			except Exception as e:
+				replies.append((f'Could not get orders. (Exception: {e})', None))
+		
+		# /cancel
 		elif msg["text"] == "/cancel" and self.buying:
 			self.buying = None
-			replies.append(("Cancelled buy.", None))
+			self.selling = None
+			replies.append(("Cancelled order.", None))
+		# Buy amount specified
 		elif self.buying:
 			try:
 				amount = float(msg["text"])
-				# Buy
-				pair, ask, quantity = self.bittrex_utils.buy_in_btc(self.buying, amount)
-				reply = (f'Buying {quantity} {self.buying} @ {"{0:.8f}".format(ask)} BTC for {amount} BTC', None)
-				replies.append(reply)
-			except Exception as e:
-				replies.append(("Please enter a valid number (up to 8 decimal places) e.g.", None))
+				# Create buy order
+				pair, quantity, price = self.bittrex_utils.prepare_btc_buy(self.buying, amount)
+				
+				# Create keyboard options (confirm and cancel)
+				# Data scheme - buy:confirm_SYMBOL:amount:pair:quantity:price
+				data = [str(x) for x in (amount, pair, quantity, price)]
+				buttons = [
+					InlineKeyboardButton(text=f'Confirm', callback_data=f'buy:confirm_{self.buying}:{":".join(data)}'),
+					InlineKeyboardButton(text=f'Cancel', callback_data=f'buy:cancel_{self.buying}')
+				]
+				keyboard = InlineKeyboardMarkup(inline_keyboard=[buttons])
+				replies.append((f'Buy {quantity} {self.buying} @ {"{0:.8f}".format(price)} BTC for {amount}', keyboard))
+					
+			except ValueError as e:
+				# Error converting value
+				replies.append(("Please enter a valid amount (up to 8 decimal places) e.g.", None))
 				replies.append(("0.00000001", None))
-		
+			except Exception as e:
+				# Exception in bittrex_utils
+				replies.append((f'Exception: {e}. Please try again.', None))
+		elif self.selling:
+			try:
+				quantity, price = [float(x) for x in msg["text"].split(",")]
+				# Create buy order
+				amount = quantity * price
+				pair = f'BTC-{self.selling}'
+
+				# Create keyboard options (confirm and cancel)
+				# Data scheme - buy:confirm_SYMBOL:btc_amount:pair:quantity:price
+				data = [str(x) for x in (amount, pair, quantity, price)]
+				buttons = [
+					InlineKeyboardButton(text=f'Confirm', callback_data=f'sell:confirm_{self.selling}:{":".join(data)}'),
+					InlineKeyboardButton(text=f'Cancel', callback_data=f'sell:cancel_{self.selling}')
+				]
+				keyboard = InlineKeyboardMarkup(inline_keyboard=[buttons])
+				replies.append((f'Sell {quantity} {self.selling} @ {"{0:.8f}".format(price)} BTC for {amount}', keyboard))
+					
+			except ValueError as e:
+				# Error converting value
+				replies.append(("Format: QUANTITY,PRICE e.g. 10000,0.00000050", None))
+			except Exception as e:
+				# Exception in bittrex_utils
+				replies.append((f'Exception: {e}. Please try again.', None))
+
 		return replies
 
-	def get_query_replies(self, query_data):
+	def get_query_replies(self, query_data, query_id=None):
 		query_type, coin = query_data.split("_")
 
 		replies = []
@@ -70,8 +149,69 @@ class TelegramBot:
 			replies.append((message, keyboard))
 		elif query_type == "buy":
 			self.buying = coin
-			replies.append((f"Send me an amount in BTC or /cancel ({self.bittrex_utils.get_available_balance()} BTC available)", None))
+			replies.append((f'Send me an amount in BTC or /cancel ({self.bittrex_utils.get_available_balance("BTC")} BTC available)', None))
+		elif query_type == "buy:confirm":
+			# Data scheme - buy:confirm_SYMBOL:amount:pair:quantity:price
+			coin, amount, pair, quantity, price = [x for x in coin.split(":")]
+			amount = float(amount)
+			quantity = float(quantity)
+			price = float(price)
+			
+			# Buy
+			try:
+				uuid = self.bittrex_utils.create_buy_order(pair, quantity, price)
+				# Selling option keyboard
+				# Data scheme - buy:confirm_SYMBOL:amount:pair:quantity:price
+				buttons = [
+					InlineKeyboardButton(text=f'Create sell order', callback_data=f'sell_{pair.split("-")[1]}:{uuid}')
+				]
+				keyboard = InlineKeyboardMarkup(inline_keyboard=[buttons])
 
+				replies.append((f'Buying {quantity} {coin} @ {"{0:.8f}".format(price)} BTC for total {amount} BTC.\n\nOrder UUID: {uuid}', None))
+				replies.append((f'Order management:\n/orderstatus ORDER_UUID - Order status\n/cancelorder ORDER_UUID - Cancel order\n/getopenorders - Get open orders', None))
+			except Exception as e:
+				replies.append((f'An error ocurred. Please try again (Exception: {e})', None))
+
+			self.buying = None
+		elif query_type == "buy:cancel":
+			self.buying = None
+			replies.append(("Cancelled order.", None))
+		elif query_type == "sell":
+			try:
+				coin, uuid = coin.split(":")
+				buy_order = self.bittrex_utils.my_bittrex.get_order(uuid)["result"]
+				if not buy_order["IsOpen"]:
+					replies.append((f'Send me the quantity and amount of {coin} or /cancel ({self.bittrex_utils.get_available_balance(coin)} {coin} available)\nLast price: {"{0:.8f}".format(self.bittrex_utils.get_last(coin))}\n\nFormat: QUANTITY,PRICE e.g. 10000,0.00000050', None)) 
+					self.selling = coin
+				else:
+					self.bot.answerCallbackQuery(query_id, text='Buy order has not yet completed. Try again.')
+			except Exception as e:
+				replies.append((f'An error ocurred. (Exception: {e})', None))
+		elif query_type == "sell:confirm":
+			# Data scheme - sell:confirm_SYMBOL:amount:pair:quantity:price
+			coin, amount, pair, quantity, price = [x for x in coin.split(":")]
+			amount = float(amount)
+			quantity = float(quantity)
+			price = float(price)
+			
+			# Sell
+			uuid = self.bittrex_utils.create_sell_order(pair, quantity, price)
+			
+			# Selling option keyboard
+			replies.append((f'Selling {quantity} {coin} @ {"{0:.8f}".format(price)} BTC for total {amount} BTC.\n\nOrder UUID: {uuid}', None))
+			replies.append((f'Order management:\n/orderstatus ORDER_UUID - Order status\n/cancelorder ORDER_UUID - Cancel order\n/getopenorders - Get open orders', None))
+			
+			self.selling = None
+		elif query_type == "sell:cancel":
+			self.selling = None
+			replies.append(("Cancelled order.", None))
+
+		# Answer callback if it hasn't already been answered
+		try:
+			self.bot.answerCallbackQuery(query_id, text='Got it')
+		except:
+			pass
+			
 		return replies
 
 	def chat_handler(self, msg):
@@ -102,11 +242,10 @@ class TelegramBot:
 			self.bot.sendMessage(chat_id, "Not authenticated. Send me this bot token to authenticate this chat.")
 			return
 
-		self.bot.answerCallbackQuery(query_id, text='Got it')
-
-		replies = self.get_query_replies(query_data)
+		replies = self.get_query_replies(query_data, query_id=query_id)
 
 		for reply in replies:
+			print(reply)
 			messsage, keyboard = reply
 			self.bot.sendMessage(self.chat_id, messsage, reply_markup=keyboard)
 	
