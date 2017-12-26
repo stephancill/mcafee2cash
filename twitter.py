a72c19e18f6538a6005ff839e5b82feb6665ad79
Stephan Cilliers <stephanus.cilliers@gmail.com>
2017-12-26 17:31:37 +0200

    Added tweet media inspection with OCR

--- twitter.py
+++ twitter.py
@@ -1,3 +1,7 @@
+import io
+from PIL import Image
+import pytesseract
+import requests
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
@@ -20,8 +24,12 @@ class TwitterListener(StreamListener):
 
 	def on_data(self, data):
 		tweet_json = json.loads(data)
-		if tweet_json["user"]["id_str"] in FOLLOW_IDS:
-			self.callback(tweet_json)
+		try: 
+			if tweet_json["user"]["id_str"] in FOLLOW_IDS:
+				print(tweet_json["text"])
+				self.callback(tweet_json)
+		except:
+			pass
 		
 class Twitter:
 	def __init__(self, tweet_callback=lambda x, y, z: x):
@@ -38,7 +46,30 @@ class Twitter:
 	def handle_tweet(self, tweet_json):
 		screen_name = tweet_json["user"]["screen_name"]
 		id = tweet_json["id_str"]
-		text = tweet_json["text"]
+		text = tweet_json["text"].replace("\\", "")
+
+		# Get media if present
+		try:
+			urls = [x["media_url"].replace("\\", "") for x in tweet_json["entities"]["media"] if x["type"] == "photo"]
+			for url in urls:
+				response = requests.get(url)
+				img = Image.open(io.BytesIO(response.content))
+				# Extract text from image
+				img_text = pytesseract.image_to_string(img)
+				text += f' . {img_text}'
+		except KeyError:
+			pass
+
 		link = f'https://twitter.com/{screen_name}/status/{id}'
-		print(tweet_json)
-		self.tweet_callback(text, screen_name, link)
+
+		try:
+			self.tweet_callback(text, screen_name, link)
+		except:
+			pass
+
+if __name__ == "__main__":
+	import time
+	twitter = Twitter()
+
+	while True:
+		time.sleep(1)
